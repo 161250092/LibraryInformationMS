7f96bede03b9130a1ebdf4101bdbd306ffbd8f59
Examinee <examinee@smail.nju.edu.cn>
2019-01-25 21:59:16 +0800

    rename

--- src/main/java/cn/tycoding/controller/userContoller/ReadOnlineController.java
+++ src/main/java/cn/tycoding/controller/userContoller/ReadOnlineController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class readOnlineController {
+public class ReadOnlineController {
     public Context context;
 
     @RequestMapping("/pdf")
